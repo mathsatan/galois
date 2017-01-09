519336e8db4b129c3f4b36b4edd7cf2ded02995c
max <mathsatan@yandex.ru>
2017-01-10 01:04:19 +0200

    asd

--- src/algebra/GaloisField.scala
+++ src/algebra/GaloisField.scala
@@ -55,6 +55,8 @@ class GaloisField(_initBitNumber: Int = FiniteField.DEFAULT_BIT_NUMBER) extends 
       case _ => false
     }
 
+    override def hashCode(): Int = this.p.polynomial.hashCode()
+    
     override protected val p = Polynomial(_p)
     /**
       * Copy constructor
