2a96409e47194d034db3030da349da594057de68
max <mathsatan@yandex.ru>
2017-01-12 22:37:43 +0200

    fixed 2

--- src/Main.scala
+++ src/Main.scala
@@ -2,17 +2,17 @@ import algebra._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val n = 5
+    val n = 6
     print(IrreduciblePolynomials(n))
-    val gf: GaloisField = GaloisField(n)
+    /*val gf: GaloisField = GaloisField(n)
 		for(i <- 1 until (1 << n)){
       for(j <- 1 until (1 << n)){
         val a = gf.createPolynomial(i)
         val b = gf.createPolynomial(j)
         val c = (a+b)-b
         if (a != c)
-        print(String.format("Error: $a not equal $c\n"))
+        print("Error")
       }
-		}
+		}*/
   }
 }
--- src/algebra/IrreduciblePolynomials.scala
+++ src/algebra/IrreduciblePolynomials.scala
@@ -23,20 +23,22 @@ object IrreduciblePolynomials{
 		true
 	}
 
-	def calcIrreducible(_deg :Int): List[Int] = {
-    def calc(deg :Int): List[Int] = {
+	def calcIrreducible(_deg: Int): List[Int] = {
+    def calc(deg: Int): List[Int] = {
       if (deg == 1) return List[Int](2, 3)
       // d > 2
       var resultList = ListBuffer[Int]()
       // generate all polynomials of degree d
       val n = 1 << deg
-      val list: List[Int] = calc(deg >> 1)
+      val nd = if (_deg.equals(deg)) deg >> 1 else deg - 1
+      val list: List[Int] = calc(nd)
       for(i <- 0 until n){
         val t = i ^ n		// polynomial of P set, for testing
         if (check(t, list)) resultList += t
       }
       resultList.toList ::: list
     }
+    if (_deg < 1) return Nil
     calc(_deg).filter(_ >= (1 << _deg))
 	}
 
